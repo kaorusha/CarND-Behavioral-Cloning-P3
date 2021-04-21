ff6ff56138c4bc188e8f5dac9bfe658462fe7f62
kaorusha <hsiehtzushin@gmail.com>
2021-04-21 04:09:09 +0000

    modify file directory

--- loader.py
+++ loader.py
@@ -3,7 +3,7 @@ import cv2
 import numpy as np
 
 lines = []
-with open('../data/driving_log.csv') as csvfile:
+with open('data/driving_log.csv') as csvfile:
     reader = csv.reader(csvfile)
     for line in reader:
         lines.append(line)
@@ -13,7 +13,7 @@ measurements = []
 for line in lines:
     source_path = line[0]
     filename = source_path.split('/')[-1]
-    current_path = '../data/IMG/' + filename
+    current_path = 'data/IMG/' + filename
     image = cv2.imread(current_path)
     images.append(image)
     measurement = float(line[3])
